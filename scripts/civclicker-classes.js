627623662f1c6daa01509f1101bb62705353eca9
Olle Harstedt <olleharstedt@yahoo.com>
2017-04-12 14:47:24 +0200

    Move VersionData class to own file

--- scripts/civclicker-classes.js
+++ scripts/civclicker-classes.js
@@ -1,36 +1,4 @@
 /**
- * Class for version.
- *
- * @param {number} major
- * @param {number} minor
- * @param {number} sub
- * @param {string} mod
- */
-function VersionData(major,minor,sub,mod) {
-	this.major = major;
-	this.minor = minor;
-	this.sub = sub;
-	this.mod = mod;
-}
-
-/**
- * Returns number representation of number.
- * @return {number}
- */
-VersionData.prototype.toNumber = function() {
-  return this.major*1000 + this.minor + this.sub/1000;
-};
-
-/**
- * Returns string of version.
- * @return {string}
- */
-VersionData.prototype.toString = function() {
-  return String(this.major) + "."
-    + String(this.minor) + "." + String(this.sub) + String(this.mod);
-};
-
-/**
  * @constructor
  * @param {object} props Properties
  * @param {boolean} asProto
