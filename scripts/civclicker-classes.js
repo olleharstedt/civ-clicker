88b018734eacfa3b0666f691f57bbc854a8c6e80
Olle Harstedt <olleharstedt@yahoo.com>
2017-04-12 14:54:14 +0200

    Move Resource class to own file

--- scripts/civclicker-classes.js
+++ scripts/civclicker-classes.js
@@ -1,43 +1,4 @@
 /**
- * Resource class
- * @constructor
- * @param {object} props - Properties
- */
-function Resource(props)
-{
-  // Prevent accidental namespace pollution
-	if (!(this instanceof Resource)) {
-    return new Resource(props);
-  }
-	CivObj.call(this,props);
-	copyProps(this,props,null,true);
-	// Occasional Properties: increment, specialChance, net
-	return this;
-}
-
-/**
- * @property {number} progressFactor - With how much each resource element should be multiplied when calculating progress time
- */
-Resource.prototype = new CivObj({
-	constructor: Resource,
-	type: "resource",
-	// 'net' accessor always exists, even if the underlying value is undefined for most resources.
-	get net() { 
-		if (typeof this.data.net !== "number") {
-			console.warn(".net not a number");
-			return 0;
-		}
-		return this.data.net; 
-	},
-	set net(value) { this.data.net = value; },
-	increment: 0,
-	specialChance: 0,
-	specialMaterial: "",
-	activity: "gathering", //I18N
-  progressFactor: 1
-},true);
-
-/**
  * @constructor
  * @param {object} props - Properties
  */
