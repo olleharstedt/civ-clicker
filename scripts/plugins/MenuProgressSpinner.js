b6daa880e0718c2dec460ed4f65e78e73686c8e0
Olle Harstedt <olleharstedt@yahoo.com>
2017-04-19 17:41:59 +0200

    Add loading spinner for upgrades

--- scripts/plugins/MenuProgressSpinner.js
+++ scripts/plugins/MenuProgressSpinner.js
@@ -13,17 +13,32 @@ CivClicker.MenuProgressSpinner = (function() {
 
   return {
     /**
-     * Init this module.
+     * Init this plugin.
      * @return
      */
     init: function() {
       // Setup building spinner.
-      var buildingSpinner = new CivClicker.ProgressSpinner('#buildings-sidemenu-loader', 'building');
+      var buildingSpinner = new CivClicker.ProgressSpinner(
+        '#buildings-sidemenu-loader',
+        'building'
+      );
       CivClicker.Events.subscribe('global.doPurchase.success', function(info) {
         buildingSpinner.onPurchaseSuccess(info);
       });
       CivClicker.Events.subscribe('global.tick', function() {
         buildingSpinner.onTick();
+      });
+
+      // Setup upgrade spinner.
+      var upgradeSpinner = new CivClicker.ProgressSpinner(
+        '#upgrades-sidemenu-loader',
+        'upgrade'
+      );
+      CivClicker.Events.subscribe('global.doPurchase.success', function(info) {
+        upgradeSpinner.onPurchaseSuccess(info);
+      });
+      CivClicker.Events.subscribe('global.tick', function() {
+        upgradeSpinner.onTick();
       });
     }
   };
