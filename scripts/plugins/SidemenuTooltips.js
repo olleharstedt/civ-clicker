0ac6e7f8b4559e7b7bfeb1e7538a60cfebefc5aa
Olle Harstedt <olleharstedt@yahoo.com>
2017-04-26 00:27:28 +0200

    Add tooltip for population sidemenu

--- scripts/plugins/SidemenuTooltips.js
+++ scripts/plugins/SidemenuTooltips.js
@@ -10,41 +10,83 @@ var CivClicker = CivClicker || {};
  */
 CivClicker.SidemenuTooltips = (function() {
   var templates = {};
+
+  /**
+   * Resource tooltip
+   */
+  function initResourceTooltip() {
+    $.get('templates/tooltips/sidemenu-resources.html', function(template) {
+      templates.resources = template;
+    }).then(function () {
+
+      // toggle is used to decide if we can use .tooltip-inner class to change content.
+      // Necessary to update tooltip content when it's shown.
+      let toggle = false;
+      $('#sidemenu-resources').on('show.bs.tooltip', () => { toggle = true; });
+      $('#sidemenu-resources').on('hide.bs.tooltip', () => { toggle = false; });
+
+      CivClicker.Events.subscribe('global.tick', () => {
+
+        let s = Mustache.to_html(
+          templates.resources,
+          {
+            civData: civData,
+            round: () =>
+            // Implicit return
+            (val, render) => {
+              let number = render(val);
+              return Math.round(number * 100) / 100;
+            }
+          }
+        );
+
+        $('#sidemenu-resources').attr('title', s).tooltip('fixTitle');
+
+        if (toggle) {
+          $('.tooltip-inner').html(s);
+        }
+      });
+    });
+  }
+
+  /**
+   * Population tooltip
+   */
+  function initPopulationTooltip() {
+    // Population menu
+    $.get('templates/tooltips/sidemenu-population.html', (template) => {
+      templates.population = template;
+    }).then(() => {
+      CivClicker.Events.subscribe('global.tick', () => {
+
+        let s = Mustache.to_html(
+          templates.population,
+          {
+            population: population,  // Global var
+            unitData: unitData,
+            ucfirst: () => {
+              return (s, render) => {
+                let rendered = render(s);
+                return rendered.charAt(0).toUpperCase() + rendered.slice(1);
+              };
+            }
+          }
+        );
+        $('#sidemenu-population')
+        .attr('title', s)
+        .tooltip('fixTitle');
+
+        //if (toggle) {
+        //$('.tooltip-inner').html(s);
+        //}
+      });
+    });
+  }
+
   return {
     init: function() {
-      $.get('templates/tooltips/sidemenu-resources.html', function(template) {
-        templates.resources = template;
-      }).then(function () {
-
-        // toggle is used to decide if we can use .tooltip-inner class to change content.
-        // Necessary to update tooltip content when it's shown.
-        let toggle = false;
-        $('#sidemenu-resources').on('show.bs.tooltip', () => { toggle = true; });
-        $('#sidemenu-resources').on('hide.bs.tooltip', () => { toggle = false; });
-
-        CivClicker.Events.subscribe('global.tick', () => {
-
-          let s = Mustache.to_html(
-            templates.resources,
-            {
-              civData: civData,
-              round: () =>
-                // Implicit return
-                (val, render) => {
-                  let number = render(val);
-                  return Math.round(number * 100) / 100;
-                }
-            }
-          );
-          $('#sidemenu-resources')
-            .attr('title', s)
-            .tooltip('fixTitle');
-
-          if (toggle) {
-            $('.tooltip-inner').html(s);
-          }
-        });
-      });
+      initResourceTooltip();
+      initPopulationTooltip();
     }
   };
 })();
