1960b26c57074529f5ca44d86fcb49e5a600885a
Olle Harstedt <olleharstedt@yahoo.com>
2017-04-30 02:07:17 +0200

    Remove useless console code

--- scripts/civclicker.js
+++ scripts/civclicker.js
@@ -3648,25 +3648,8 @@ $(function () {
         };
 
       } else {
-        // Disable console.
-				(function() {
-						try {
-								var $_console$$ = console;
-								Object.defineProperty(window, "console", {
-										get: function() {
-												if ($_console$$._commandLineAPI)
-														throw "Sorry, for security reasons, the script console is deactivated on netflix.com";
-												return $_console$$
-										},
-										set: function($val$$) {
-												$_console$$ = $val$$
-										}
-								})
-						} catch ($ignore$$) {
-						}
-				})();
+        // TODO
       }
-
     },
     error: () => {
       // Die horribly.
