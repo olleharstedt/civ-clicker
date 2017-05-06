b0c60ec04ed95854b4c991e64c70fe233a16d8bd
Olle Harstedt <olleharstedt@yahoo.com>
2017-05-06 23:21:01 +0200

    Move increment resource to Resource class

--- scripts/classes/Resource.js
+++ scripts/classes/Resource.js
@@ -53,6 +53,53 @@ Resource.prototype = new CivObj({
       // Internal error.
     }
   },
+
+  /**
+   * This function is called every time a player clicks on a primary resource button
+   * @param {string} objId
+   */
+  incrementResource: function(objId) {
+    var purchaseObj = civData[objId];
+    var numArmy = 0;
+
+    if (!purchaseObj) {
+      console.log('Unknown purchase: '+ objId);
+      return;
+    }
+
+    unitData.forEach(function(elem) { 
+      if ((elem.alignment == 'player') && (elem.species=='human')
+          && (elem.combatType) && (elem.place == 'home')) { 
+        numArmy += elem.owned; 
+      } 
+    }); // Nationalism adds military units.
+
+    purchaseObj.owned += purchaseObj.increment 
+    + (purchaseObj.increment * 9 * (civData.civilservice.owned)) 
+    + (purchaseObj.increment * 40 * (civData.feudalism.owned)) 
+    + ((civData.serfs.owned) * Math.floor(Math.log(civData.unemployed.owned * 10 + 1))) 
+    + ((civData.nationalism.owned) * Math.floor(Math.log(numArmy * 10 + 1)));
+
+    //Handles random collection of special resources.
+    var specialChance = purchaseObj.specialChance;
+    if (specialChance && purchaseObj.specialMaterial && civData[purchaseObj.specialMaterial]) {
+      if ((purchaseObj === civData.food) && (civData.flensing.owned))    { specialChance += 0.1; }
+      if ((purchaseObj === civData.stone) && (civData.macerating.owned)) { specialChance += 0.1; }
+      if (Math.random() < specialChance) {
+        var specialMaterial = civData[purchaseObj.specialMaterial];
+        var specialQty =  purchaseObj.increment * (1 + (9 * (civData.guilds.owned)));
+        specialMaterial.owned += specialQty;
+        gameLog('Found ' + specialMaterial.getQtyName(specialQty) + ' while ' + purchaseObj.activity); // I18N
+      }
+    }
+    //Checks to see that resources are not exceeding their limits
+    if (purchaseObj.owned > purchaseObj.limit) {
+      purchaseObj.owned = purchaseObj.limit;
+    }
+
+    ui.find('#clicks').innerHTML = prettify(Math.round(++curCiv.resourceClicks));
+    updateResourceTotals(); //Update the page with totals
+  },
   increment: 0,
   specialChance: 0,
   specialMaterial: '',
