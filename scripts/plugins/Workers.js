ef5ada08e41a6491fe70787e3cac63f0dfa58459
Olle Harstedt <olleharstedt@yahoo.com>
2017-05-28 20:07:31 +0200

    Cleanup

--- scripts/plugins/Workers.js
+++ scripts/plugins/Workers.js
@@ -5,31 +5,27 @@
  * Plugin for workers ticking.
  * @prop object tickSub
  */
-CivClicker.plugins.Workers = (function() {
+CivClicker.plugins.Workers = new (class WorkersPlugin {
+  init() {
+    this.tickSub = CivClicker.Events.subscribe('global.tick', () => {
 
-  // Return an instance of the actual plugin.
-  return new (class WorkersPlugin {
-    init() {
-      this.tickSub = CivClicker.Events.subscribe('global.tick', () => {
-	
-        // The "net" values for special resources are just running totals of the
-        // adjustments made each tick; as such they need to be zero'd out at the
-        // start of each new tick.
-        clearSpecialResourceNets();
+      // The "net" values for special resources are just running totals of the
+      // adjustments made each tick; as such they need to be zero'd out at the
+      // start of each new tick.
+      clearSpecialResourceNets();
 
-        // Production workers do their thing.
-        unitData.forEach((unit) => {
-          if (unit instanceof WorkUnit) {
-            unit.doWork();
-          }
-        });
+      // Production workers do their thing.
+      unitData.forEach((unit) => {
+        if (unit instanceof WorkUnit) {
+          unit.doWork();
+        }
       });
-    }
+    });
+  }
 
-    unload() {
-      if (this.tickSub) {
-        this.tickSub.remove();
-      }
+  unload() {
+    if (this.tickSub) {
+      this.tickSub.remove();
     }
-  });
-})();
+  }
+});
