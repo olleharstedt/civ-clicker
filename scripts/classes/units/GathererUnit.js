29ab47290e5cda7b6457c39cbdb729a11f500ad3
Olle Harstedt <olleharstedt@yahoo.com>
2017-06-11 12:47:43 +0200

    Different calc from clicking gather and unit gather

--- scripts/classes/units/GathererUnit.js
+++ scripts/classes/units/GathererUnit.js
@@ -25,13 +25,24 @@ class GathererUnit extends WorkUnit {
   }
 
   doWork() {
+    /*
     const inc = civData.gather.incrementResource(
       'gather',
       civData.gatherer.owned
     );
     civData.food.net += inc;
+    */
     //const inc = 2 * civData.gatherer.owned; 
     //civData.food.owned += inc;
+
+    const owned = civData.gatherer.owned;
+    const foodChance = Math.pow(0.9, owned);
+    if (Math.random() > foodChance) {
+      const found = owned;
+      const formatChance = Math.round(((1 - foodChance) * 100)) + '%';
+      civData.food.owned += found;
+      gameLog(`Found ${found} food while gathering (chance ${formatChance})`);
+    }
   }
 
 }
