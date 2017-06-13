0fe03bd58c926c484592cb0444d6547b130def82
Olle Harstedt <olleharstedt@yahoo.com>
2017-06-13 23:49:06 +0200

    Small k for kilo

--- scripts/civclicker.js
+++ scripts/civclicker.js
@@ -3201,7 +3201,7 @@ function shortify(num) {
   } else if (num >= 1000000 || num <= -1000000) {
     return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
   } else if (num >= 1000 || num <= -1000) {
-    return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
   }
 
   if (Math.round(num) == num) {
