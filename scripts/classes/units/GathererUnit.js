cec9d0935f7889af812fd949d6641dbb4764fb28
Olle Harstedt <olleharstedt@yahoo.com>
2017-06-18 23:08:45 +0200

    Add wood and stone chance to gathering

--- scripts/classes/units/GathererUnit.js
+++ scripts/classes/units/GathererUnit.js
@@ -47,6 +47,20 @@ class GathererUnit extends WorkUnit {
       civData.food.owned += found;
       gameLog(`Found ${found} food while gathering (chance ${formatChance})`);
     }
+
+    const stoneChance = Math.pow(0.99, owned);
+    if (Math.random() > stoneChance) {
+      civData.stone.owned += 1;
+      const formatChance = Math.round(((1 - stoneChance) * 100)) + '%';
+      gameLog(`Found stone while gathering (chance ${formatChance})`);
+    }
+
+    const woodChance = Math.pow(0.99, owned);
+    if (Math.random() > woodChance) {
+      civData.wood.owned += 1;
+      const formatChance = Math.round(((1 - woodChance) * 100)) + '%';
+      gameLog(`Found wood while gathering (chance ${formatChance})`);
+    }
   }
 
 }
