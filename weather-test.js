7d549819c2a55b5bd9c89c7ac0455bc80448cf1a
Olle Harstedt <olleharstedt@yahoo.com>
2017-09-17 22:22:37 +0200

    Add temperature based on Climak (min only)

--- weather-test.js
+++ weather-test.js
@@ -1,5 +1,51 @@
 $(function () {
 
+  // NB: Also in Weather.js.
+  function gaussian(mean, stdev) {
+    var y2;
+    var use_last = false;
+    return function() {
+      var y1;
+      if(use_last) {
+        y1 = y2;
+        use_last = false;
+      } else {
+        var x1, x2, w;
+        do {
+          x1 = 2.0 * Math.random() - 1.0;
+          x2 = 2.0 * Math.random() - 1.0;
+          w  = x1 * x1 + x2 * x2;               
+        } while( w >= 1.0);
+        w = Math.sqrt((-2.0 * Math.log(w))/w);
+        y1 = x1 * w;
+        y2 = x2 * w;
+        use_last = true;
+      }
+
+      var retval = mean + stdev * y1;
+      if(retval > 0) 
+        return retval;
+      return -retval;
+    };
+  }
+
+  let tempDry = (function() {
+    let A = gaussian(6.83, 0.785);
+    let B = gaussian(10.08, 0.595);
+    const C = 111.8;
+    let D = gaussian(-0.545, 0.693);
+    const E = 130.91;
+
+    return function(date) {
+      return A() 
+        + (B() * Math.sin((date - C) * ((2 * Math.PI) / 100)))
+        + (D() * Math.sin((date - E) * ((2 * Math.PI) / 50)))
+      ;
+    };
+
+  })();
+
+
   // Return number of rain streaks.
   function getNumberOfStreaks(list) {
     let streaks = 0;
@@ -27,13 +73,16 @@ $(function () {
 
   let weatherList = [];
   const totalDays = 100;
+  let daysList = [];
   let precList= [];
   let totalPrec = 0;
   let precSeasonList = [];
   let cosList = [];
   let wetDays = 0;
   let dryDays = 0;
+  let tempList = [];
   for (let i = 0; i < totalDays; i++) {
+    daysList.push(i);
     CivClicker.plugins.Weather.decideWetOrDry();
     if (CivClicker.plugins.Weather.wetOrDry == 2) {
       // 2 == DRY
@@ -50,6 +99,7 @@ $(function () {
     const cos = Math.cos(i / (50/Math.PI)) /2 + 1;
     cosList.push(cos);
     precSeasonList.push(prec * cos);
+    tempList.push(tempDry(i));
   }
 
   var streaks = getNumberOfStreaks(weatherList);
@@ -109,7 +159,7 @@ $(function () {
   precChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: precList,
+      labels: daysList,
       datasets: [
         {
           label: 'Precipitation during 100 "days"',
@@ -129,11 +179,28 @@ $(function () {
   precChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: precSeasonList,
+      labels: daysList,
       datasets: [
         {
           label: 'Seasonal precipitation during 100 "days"',
           data: precSeasonList
+        }
+      ]
+    },
+    options: {
+      'label': 'bla'
+    }
+  });
+
+  ctx = document.getElementById('temperature').getContext('2d');
+  let _tmp = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: daysList,
+      datasets: [
+        {
+          label: 'Temperature',
+          data: tempList
         }
       ]
     },
