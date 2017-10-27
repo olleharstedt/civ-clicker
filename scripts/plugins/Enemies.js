5943917130d4662666dcd0cf12b463ddc049ca55
SkyHawkB <danielmey626@gmail.com>
2017-10-26 20:05:20 -0400

    Create Enemies.js
    create file

---
+++ scripts/plugins/Enemies.js
@@ -0,0 +1,54 @@
+/* @flow */
+'use strict';
+
+/**
+ * Deals with population health.
+ * @property {number} temperature - Fetched from the weather plugin.
+ */
+CivClicker.plugins.Enemies = (() => {
+  return new (class EnemiesPlugin {
+
+    /**
+     *
+     */
+    constructor() {
+      this.isNight = false; // start at day
+      this.hearthsOwned = curCiv.hearths.owned;
+    }
+
+    /**
+     * Tick.
+     */
+    tick() {
+      /*
+      / Get chance for wolves to attack
+      */
+    }
+
+    /**
+     * Init plugin.
+     */
+    init() {
+      this.tickSub = CivClicker.Events.subscribe('global.tick', () => {
+        this.tick();
+      });
+      
+      // Listen to the DayNight plugin.
+      this.daySub = CivClicker.Events.subscribe('daynight.day.begin', () => {
+        this.isNight = false;
+      });
+      this.nightSub = CivClicker.Events.subscribe('daynight.night.begin', () => {
+        this.isNight = true;
+      });
+      
+      // Update number of hearths on successful and finished purchase.
+      this.buySuccessSub = CivClicker.Events.subscribe('global.doPurchase.success', () => {
+        this.hearthsOwned = curCiv.hearths.owned;
+      });
+      this.buyFinishedSub = CivClicker.Events.subscribe('global.doPurchase.finished', () => {
+        this.hearthsOwned = curCiv.hearths.owned;
+      });
+    }
+
+  });
+})();
