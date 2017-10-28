ac800cbafa0eabd8eadc9af19e18e10c35cd10ee
Olle Harstedt <olleharstedt@yahoo.com>
2017-10-28 16:07:45 +0200

    Clean up FreeLand HTML

--- scripts/classes/buildings/FreeLand.js
+++ scripts/classes/buildings/FreeLand.js
@@ -9,15 +9,13 @@ class FreeLand extends Building {
    */
   getPurchaseRowHtml() {
     const name = ucfirst(this.singular);
-    const reqText = getReqText(this.require);
-    const progressBarId = this.getProgressBarCellId();
     return `
       <tr data-target='${this.id}'>
         <td>${name}</td>
-        <td id='${progressBarId}' style='width: 100px;'></td>
+        <td></td>
         <td id='${this.singular}-owned-cell'>${this.owned}</td>
         <td></td>
-        <td><span class='text-muted'>${reqText}</span></td>
+        <td></td>
         <td><span class='text-muted'>${this.effectText}</span></td>
       </tr>
     `;
