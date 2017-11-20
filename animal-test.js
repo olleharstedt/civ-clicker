7271a90304d812c73c725ec82b12d4cc6400028c
Olle Harstedt <olleharstedt@yahoo.com>
2017-11-20 23:02:39 +0100

    Tuning predator-prey test

--- animal-test.js
+++ animal-test.js
@@ -14,6 +14,10 @@ $(function () {
 			}
 			// Euler method to compute next time step
 			for(var j=0; j<n; j++) {
+        //console.log(x.get([j]));
+        //console.log(dt);
+        //console.log(dxdt[j]);
+        //console.log(math.multiply(dxdt[j], dt));
 				x.set([j], math.add(x.get([j]), math.multiply(dxdt[j], dt)));
 			}
 			result.push(x.clone());
@@ -26,6 +30,8 @@ $(function () {
 	math.import({ndsolve:ndsolve});
 
 	// Create a math.js context for our simulation. Everything else occurs in the context of the expression parser!
+  
+  var doTheThing = function() {
 
 	var sim = math.parser();
 
@@ -121,5 +127,82 @@ $(function () {
 		}
 
 	});
+  }
+
+  //doTheThing();
+
+  // Test 1
+
+	const sim2 = math.parser();
+	sim2.eval("alpha = 0.3");
+	sim2.eval("beta = 0.01");
+	sim2.eval("delta = 0.01");
+	sim2.eval("gamma = 1");
+	sim2.eval("dxdt(x, y) = alpha * x - beta * x * y");
+	sim2.eval("dydt(x, y) = delta * x * y - gamma * y");
+	sim2.eval("dt = 0.1");                // Simulation timestep
+	sim2.eval("x0 = 100");
+	sim2.eval("y0 = 10");
+	sim2.eval("tfinal = 100");          // Simulation duration
+
+	// Again, remember to maintain the same variable order in the call to ndsolve.
+	sim2.eval("result_stage1 = ndsolve([dxdt, dydt], [x0, y0], dt, tfinal)");
+	var data_stage1 =     sim2.eval("result_stage1").toArray().map(function(e) { return {x: e[0], y: e[1]}; });
+  console.log(data_stage1);
+  first = [];
+  for (let i = 0; i < data_stage1.length; i++) {
+    //data_stage1[i].x = i;
+    first.push({
+      x: i,
+      y: data_stage1[i].y
+    });
+  }
+  other = [];
+  for (let i = 0; i < data_stage1.length; i++) {
+    other.push({
+      x: i,
+      y: data_stage1[i].x
+    });
+  }
+  console.log(other);
+
+	var chart = new Chart(document.getElementById('canvas1'), {
+		type: 'line',
+		data: {
+			datasets: [{
+				label: "Stage 1",
+				data: first,
+				fill: false,
+				borderColor: "red",
+				pointRadius: 0
+			},
+      {
+				label: "Stage 2",
+				data: other,
+				fill: false,
+				borderColor: "blue",
+				pointRadius: 0
+			}
+      ]
+		},
+		options: {
+			scales: {
+				xAxes: [{
+					type: 'linear',
+					position: 'bottom'
+				}]
+			}
+		}
+  });
+
+
+  /*
+	const sim3 = math.parser();
+	sim3.eval("dydt(x) = x * dt");
+	sim3.eval("x0 = 0");
+	sim3.eval("tfinal = 100 s");          // Simulation duration
+	sim3.eval("dt = 1.0 s");                // Simulation timestep
+	sim3.eval("result_stage1 = ndsolve([dydt], [x0], dt, tfinal)");
+  */
 
 });
